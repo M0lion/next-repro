a252710fe92a4add7e724f02a49fff553aead468
Bjørn Moholt <bjorn-moholt@hotmail.com>
2022-11-22 17:12:44 +0100

    Added minimal reproduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default async function Home() {
+  try {
+    await fetch("https://jsonplaceholder.typicode.com/todos/1", {
+      cache: "no-store",
+    });
+  } catch (error) {
+    throw new Error("My error message", { cause: error });
+  }
+
+  return <></>;
 }
